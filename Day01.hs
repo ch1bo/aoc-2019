332304445d1f02ff3d0fec5a9c37ab3840eff470
Sebastian Nagel <sebastian.nagel@franka.de>
2019-12-11 19:15:46 +0100

    Implement day 01 part two

--- Day01.hs
+++ Day01.hs
@@ -1,16 +1,25 @@
 #!/usr/bin/env stack
 -- stack script --resolver lts-14.4
+{-# LANGUAGE BangPatterns#-}
 module Main where
 
 import Numeric.Natural
 
+type Module = Natural
 type Mass = Natural
 type Fuel = Natural
 
-fuelCounter :: Mass -> Fuel
-fuelCounter m = (m `div` 3) - 2
+requiredFuel :: Mass -> Fuel
+requiredFuel !m
+  | m' >= 2 = (m' - 2) + requiredFuel (m' - 2)
+  | otherwise = 0
+ where
+  m' = m `div` 3
+
+moduleFuel :: Module -> Fuel
+moduleFuel m = requiredFuel m
 
 main :: IO ()
 main = do
   modules <- fmap read . lines <$> readFile "input.txt"
-  print $ sum $ map fuelCounter modules
+  print $ sum $ map moduleFuel (modules :: [Module])
