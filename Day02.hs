a9bd468816dcbd09ca6a47a408db5b834492123d
Sebastian Nagel <sebastian.nagel@franka.de>
2019-12-13 17:29:36 +0100

    Implement day 02 part two

--- Day02.hs
+++ Day02.hs
@@ -6,26 +6,30 @@
 module Main where
 
 import Numeric.Natural
+import Control.Monad
 import Safe
 import Data.Vector ((!?), Vector, (//))
+import Data.Foldable
 
 import qualified Data.ByteString as BS
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 import qualified Data.Vector as Vector
 
+type Memory = Vector Int
+
 type Address = Int
 
-data OpCode = Add Address Address Address
-            | Multiply Address Address Address
-            | Halt
+data Instruction = Add Address Address Address
+                 | Multiply Address Address Address
+                 | Halt
 
-data IntCode = IntCode { code :: Vector Int
+data IntCode = IntCode { memory :: Memory
                        , pos :: !Address
                        } deriving Show
 
-parseOpCode :: IntCode -> Maybe (OpCode, IntCode)
-parseOpCode i = do
+parseInstruction :: IntCode -> Maybe (Instruction, IntCode)
+parseInstruction i = do
   (op, i') <- parseInt i
   case op of
     1 -> do
@@ -45,25 +49,36 @@ parseInt :: IntCode -> Maybe (Int, IntCode)
 parseInt i = (, i { pos = pos i + 1 }) <$> lookupInt (pos i) i
 
 lookupInt :: Address -> IntCode -> Maybe Int
-lookupInt a i = code i !? a
+lookupInt a i = memory i !? a
 
-runIntcode :: Vector Int -> Maybe IntCode
-runIntcode is = go $ IntCode is 0
+runIntCode :: Memory -> Maybe IntCode
+runIntCode is = go $ IntCode is 0
  where
   go i = do
-    (op, i') <- parseOpCode i
+    (op, i') <- parseInstruction i
     case op of
       Add a b r -> do
         x <- lookupInt a i
         y <- lookupInt b i
-        go $ i' { code = code i' // [(r, x + y)] }
+        go $ i' { memory = memory i' // [(r, x + y)] }
       Multiply a b r -> do
         x <- lookupInt a i
         y <- lookupInt b i
-        go $ i' { code = code i' // [(r, x * y)] }
+        go $ i' { memory = memory i' // [(r, x * y)] }
       Halt -> pure i'
+
+type Noun = Int
+type Verb = Int
+
+compute :: Memory -> Noun -> Verb -> Maybe Int
+compute m n v = runIntCode (m // [(1, n), (2, v)]) >>= lookupInt 0
 
 main :: IO ()
 main = do
-  ints <- Vector.fromList . map (read . Text.unpack) . (Text.split (== ',') . Text.decodeUtf8) <$> BS.readFile "day02-input.txt"
-  print $ runIntcode (ints // [(1, 12), (2, 2)]) >>= lookupInt 0
+  gravityAssistProgram <- Vector.fromList . map (read . Text.unpack) . (Text.split (== ',') . Text.decodeUtf8) <$> BS.readFile "day02-input.txt"
+  -- Naive search / brute force search for a solution using permutations the list monad
+  print $ do
+    noun <- [0..99]
+    verb <- [0..99]
+    guard (compute gravityAssistProgram noun verb == Just 19690720)
+    return (noun, verb, 100 * noun + verb)
