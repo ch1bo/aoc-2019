b3d6c34bf781e49ba1af15de9c7cce4a07d8d6c0
Sebastian Nagel <sebastian.nagel@ncoding.at>
2019-12-14 00:49:10 +0100

    Solve day 04 - part one

---
+++ Day04.hs
@@ -0,0 +1,24 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-14.4
+module Main where
+
+import           Control.Monad
+import           Data.List
+
+type Password = Int
+
+check :: Password -> Bool
+check p = doubles p && increasing p
+
+doubles :: Password -> Bool
+doubles p = any ((>=2) . length) $ group $ show p
+
+increasing :: Password -> Bool
+increasing p = maybe False (const True) $ foldl' go (Just '0') $ show p
+ where
+  go (Just p) c | c >= p = Just c
+  go _ _        = Nothing
+
+main :: IO ()
+main = do
+  print $ length $ filter check [136818..685979]
