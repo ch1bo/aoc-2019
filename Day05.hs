d99d5639ff9761bbf3499110c7be5aa095a3a46e
Sebastian Nagel <sebastian.nagel@ncoding.at>
2019-12-15 21:49:07 +0100

    Add IO opcodes

--- Day05.hs
+++ Day05.hs
@@ -6,16 +6,19 @@
 module Main where
 
 import           Control.Monad
+import           Control.Monad.IO.Class
 import           Control.Monad.State
 import           Data.Foldable
-import           Data.Vector         (Vector, (!), (//))
+import           Data.Vector            (Vector, (!), (//))
 import           Numeric.Natural
 import           Safe
 
-import qualified Data.ByteString     as BS
-import qualified Data.Text           as Text
-import qualified Data.Text.Encoding  as Text
-import qualified Data.Vector         as Vector
+import qualified Data.ByteString        as BS
+import qualified Data.Text              as Text
+import qualified Data.Text.Encoding     as Text
+import qualified Data.Vector            as Vector
+
+import           Debug.Trace
 
 type Memory = Vector Int
 
@@ -31,13 +34,13 @@ data IntCode = IntCode { memory :: Memory
                        , pos    :: !Address
                        } deriving Show
 
-newtype Computer a = Computer { unComputer :: State IntCode a }
-                   deriving (Functor, Applicative, Monad, MonadState IntCode)
+newtype Computer m a = Computer { unComputer :: StateT IntCode m a }
+                     deriving (Functor, Applicative, Monad, MonadState IntCode, MonadIO)
 
-runComputer :: Computer a -> IntCode -> a
-runComputer (Computer a) i = fst $ runState a i
+runComputer :: Monad m => Computer m a -> IntCode -> m a
+runComputer (Computer a) i = runStateT a i >>= \(a, i) -> return a
 
-parseInstruction :: Computer Instruction
+parseInstruction :: Monad m => Computer m Instruction
 parseInstruction = do
   op <- parseInt
   case op of
@@ -56,20 +59,20 @@ parseInstruction = do
     99 -> return Halt
     i -> error $ "unknown opcode: " ++ show i
 
-parseInt :: Computer Int
+parseInt :: Monad m => Computer m Int
 parseInt = do
   i <- get
   res <- lookupInt (pos i)
   put (i { pos = pos i + 1})
   return res
 
-lookupInt :: Address -> Computer Int
+lookupInt :: Monad m => Address -> Computer m Int
 lookupInt a = gets (\i -> memory i ! a)
 
-modifyMemory :: (Memory -> Memory) -> Computer ()
+modifyMemory :: Monad m => (Memory -> Memory) -> Computer m ()
 modifyMemory f = modify (\i -> i { memory = f $ memory i })
 
-runIntCode :: Memory -> IntCode
+runIntCode :: Memory -> IO IntCode
 runIntCode is = runComputer (go >> get) $ IntCode is 0
  where
   go = do
@@ -85,11 +88,17 @@ runIntCode is = runComputer (go >> get) $ IntCode is 0
         y <- lookupInt b
         modifyMemory (// [(r, x * y)])
         go
-      Input a -> go
-      Output a -> go
+      Input a -> do
+        liftIO $ putStr "Input: "
+        v <- read <$> liftIO getLine
+        modifyMemory (// [(a, v)])
+        go
+      Output a -> do
+        lookupInt a >>= liftIO . putStrLn . mappend "Output: " . show
+        go
       Halt -> pure ()
 
 main :: IO ()
 main = do
   diagnosticProgram <- Vector.fromList . map (read . Text.unpack) . (Text.split (== ',') . Text.decodeUtf8) <$> BS.readFile "day05-input.txt"
-  print $ runIntCode diagnosticProgram
+  print =<< runIntCode diagnosticProgram
