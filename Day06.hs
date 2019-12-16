e3ef0b3b6b61be7ad3b9f0fb518490ce0d49432f
Sebastian Nagel <sebastian.nagel@ncoding.at>
2019-12-16 01:37:36 +0100

    Refactor and solve part one

--- Day06.hs
+++ Day06.hs
@@ -6,32 +6,33 @@ module Main where
 import           Data.List
 import           Data.Map    (Map)
 import qualified Data.Map    as Map
+import           Data.Set    (Set)
+import qualified Data.Set    as Set
 import           Debug.Trace
 
-data OrbitMap = Orbits String [OrbitMap]
-              deriving Show
+type OrbitMap = Map String (Set String)
 
 parseOrbits :: [String] -> OrbitMap
-parseOrbits = foldl' (flip addOrbit) (Orbits "COM" [])
+parseOrbits = foldl' (flip addOrbit) mempty
 
 addOrbit :: String -> OrbitMap -> OrbitMap
-addOrbit s (Orbits n os)
-  | n == a = Orbits a os'
-  | otherwise = Orbits n (fmap (addOrbit s) os) -- TODO Foldable OrbitMap
+addOrbit s = Map.alter add' a
  where
   (a, (_:b)) = break (==')') s
 
-  os' = maybe (Orbits b []:os) (const os) $ find (\(Orbits n _) -> n == b) os
+  add' Nothing  = Just $ Set.singleton b
+  add' (Just s) = Just $ s <> Set.singleton b
 
 orbitCountChecksum :: OrbitMap -> Int
-orbitCountChecksum o = go 0 o 0
+orbitCountChecksum om = go 0 "COM" 0
  where
-  go depth (Orbits n []) acc = (trace $ (show depth) ++ " " ++ n ++ " " ++ (show acc)) acc + depth
-  go depth (Orbits n os) acc = (trace $ (show depth) ++ " " ++ n ++ " " ++ (show acc)) foldr (go $ depth + 1) (acc + depth) os
+  go !depth k !sum =
+    case Map.lookup k om of
+      Just os -> foldr (go (depth + 1)) (sum + depth) os
+      Nothing -> sum + depth
 
 main :: IO ()
 main = do
-  -- TODO(SN): input not sorted!
   input <- lines <$> readFile "day06-input.txt"
   -- let input = [ "COM)B"
   --             , "B)C"
@@ -46,5 +47,5 @@ main = do
   --             , "K)L"
   --             ]
   let orbits = parseOrbits input
-  print orbits
+  -- print orbits
   print $ orbitCountChecksum orbits
