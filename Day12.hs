39c7a28ee55e4d848a5b943ecbcf249c8b9c1738
Sebastian Nagel <sebastian.nagel@franka.de>
2019-12-26 10:47:57 +0100

    Start day 12 part two with a hashmap

--- Day12.hs
+++ Day12.hs
@@ -1,8 +1,14 @@
 #!/usr/bin/env stack
--- stack script --resolver lts-14.4
+-- stack script --resolver lts-14.4 --package unordered-containers
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
+import Debug.Trace
+
 import Data.List
+import Data.Foldable
+
+import qualified Data.HashMap.Lazy as Map
 
 data Moon = Moon { posX :: Int
                  , posY :: Int
@@ -35,9 +41,6 @@ simulateStep ms =
   map applyVelocity $
   [ foldr applyGravity m (ms \\ [m]) | m <- ms ]
 
-simulate :: Int -> [Moon] -> [Moon]
-simulate n ms = iterate simulateStep ms !! n
-
 totalEnergy :: [Moon] -> Int
 totalEnergy = foldr energy 0
  where
@@ -49,10 +52,34 @@ totalEnergy = foldr energy 0
 
 main :: IO ()
 main = do
-  print $ totalEnergy $ simulate 1000 ms
+  -- part one
+  print $ totalEnergy $ iterate simulateStep ms !! 1000
+  -- part two
+  print $ foldlM twoSame (0,Map.empty) $ iterate simulateStep ms
  where
+  twoSame (!i,m) ms =
+    let e = totalEnergy ms
+    in case Map.lookup e m of
+      Just ms' | any (== ms) ms' -> Left i
+      _ -> if i `mod` 1000 == 0
+           then trace (show i) Right (i+1,Map.alter (prepend ms) e m)
+           else Right (i+1,Map.alter (prepend ms) e m)
+
+  prepend a Nothing = Just [a]
+  prepend a (Just as) = Just (a:as)
+
   ms = [m1,m2,m3,m4]
   m1 = mkMoon 4 1 1
   m2 = mkMoon 11 (-18) (-1)
   m3 = mkMoon (-2) (-10) (-4)
   m4 = mkMoon (-7) (-2) 14
+
+  -- m1 = mkMoon (-1) 0 2
+  -- m2 = mkMoon 2 (-10) (-7)
+  -- m3 = mkMoon 4 (-8) 8
+  -- m4 = mkMoon 3 5 (-1)
+
+  -- m1 = mkMoon (-8) (-10) 0
+  -- m2 = mkMoon 5 5 10
+  -- m3 = mkMoon 2 (-7) 3
+  -- m4 = mkMoon 9 (-8) (-3)
