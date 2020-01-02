9ff3e824fe5e12a2e07b63deb39e2c89a27fe3b6
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-01-02 21:14:20 +0100

    Solve day 15 part one

--- day15/Main.hs
+++ day15/Main.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 module Main where
 
 import Computer
 
 import Control.Monad.State
 import Control.Monad.Identity
+import Data.List
+
+import Debug.Trace
 
 data Movement = North | South | West | East
-              deriving Show
+              deriving (Eq, Show)
 
 instance Enum Movement where
   toEnum 1 = North
@@ -23,23 +27,34 @@ instance Enum Movement where
 data Status = Wall | Moved | Found
             deriving (Enum, Show)
 
-type RC = StateT IntCode IO
-
-step :: Movement -> RC Status
-step m =
-  stepComputer (pure $ toInteger $ fromEnum m) >>= \case
+step :: IntCode -> Movement -> (Status, IntCode)
+step ic m =
+  (flip runState) ic $ stepComputer (pure $ toInteger $ fromEnum m) >>= \case
     Nothing -> error "unexpected halt"
     Just o -> return $ toEnum $ fromInteger o
+
+type Path = [Movement]
+
+next :: Path -> [Path]
+next [] = [North:[], South:[], West:[], East:[]]
+next p@(North:_) = [North:p, West:p, East:p]
+next p@(South:_) = [South:p, West:p, East:p]
+next p@(West:_) = [West:p, North:p, South:p]
+next p@(East:_) = [East:p, North:p, South:p]
+
+bfs :: IntCode -> Maybe [Movement]
+bfs ic = go $ map (ic,) $ next []
+ where
+  go [] = Nothing
+  go ((_, []):open) = go open
+  go ((ic, path):open) =
+    case step ic $ head path of
+      (Found, _) -> Just path
+      (Moved, ic') -> go $ open ++ (map (ic',) $ next path)
+      (Wall, _) -> go open
 
 main :: IO ()
 main = do
   program <- loadIntCode "day15/input.txt"
-  flip evalStateT program $ do
-    step North >>= liftIO . print
-    step North >>= liftIO . print
-    step North >>= liftIO . print
-    step North >>= liftIO . print
-    step North >>= liftIO . print
-    step North >>= liftIO . print
-    step North >>= liftIO . print
-    step North >>= liftIO . print
+  -- part one
+  print $ length <$> bfs program
